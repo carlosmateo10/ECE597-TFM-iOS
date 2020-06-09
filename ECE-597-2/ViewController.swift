cf895114216405560a81d075a2a1f26f17420a30
Carlos Mateo <carlos23p@gmail.com>
2020-06-09 04:46:09 -0700

    Initial Commit

---
+++ ECE-597-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ECE-597-2
+//
+//  Created by Carlos Mateo on 09/06/2020.
+//  Copyright © 2020 Carlos Mateo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ECE-597-2Tests/ECE_597_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ECE_597_2Tests.swift
+//  ECE-597-2Tests
+//
+//  Created by Carlos Mateo on 09/06/2020.
+//  Copyright © 2020 Carlos Mateo. All rights reserved.
+//
+
+import XCTest
+@testable import ECE_597_2
+
+class ECE_597_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
